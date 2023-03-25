2e0e048c0c02a0a31f5a830ac9289c8ff349af73
Satyanarayan pandey <satyampandey91298@gmail.com>
2023-03-26 01:59:40 +0530

    first commit

---
+++ src/main/java/com/flight/management/SvcFlightManagementApplication.java
@@ -0,0 +1,13 @@
+package com.flight.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcFlightManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcFlightManagementApplication.class, args);
+	}
+
+}
